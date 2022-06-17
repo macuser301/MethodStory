f40b32741d5045b10c89a4d6c30ebc306653be08
macuser301 <macuser30@yahoo.com>
2022-06-17 14:34:00 -0500

    initial commit

--- Program.cs
+++ Program.cs
@@ -60,8 +60,12 @@ namespace MethodStory
             Console.WriteLine($"{userName} the answer to your problem is {devide}.");
 
             Console.WriteLine($"Congratulations {userName} you passed my quiz.");
+            yourAge();
+
+
 
         }
+
 
         public static int Multiply(int num3, int num4)
         {
@@ -88,12 +92,96 @@ namespace MethodStory
         {
             int sum = 0;
 
-            for(int i = 0; i < list.Length; i++)
-            { 
-                   sum = sum + list[i];
+            for (int i = 0; i < list.Length; i++)
+            {
+                sum = sum + list[i];
             }
-            
             return sum;
+
+        }
+        static void yourAge()
+        {
+
+            var age = 33;
+            var something = ++age;
+
+            Console.WriteLine($" something = {something}");
+            Console.WriteLine($"age = {age}");
+
+            if (age > 25 || age < 40)
+            {
+                Console.WriteLine("Person is in voting age bracket");
+            }
+
+
+
+
+
+
+
+
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+
+                Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            }
+
+            // exercise 2 
+
+            double radius, area;
+            Console.WriteLine("Enter the radius of the circle");
+            radius = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI * radius * radius;
+            Console.WriteLine("Area of the Circle with radius {0} is {1}", radius, area);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+
+            var i = 3;
+            var j = 4;
+
+            var k = ++i * j++;
+            Console.WriteLine($"{k}");
+
+
+
+
+
+
+
+
+            /*
+       
+           
+            var a = 17;
+            var b = 4;
+            var div = a / b;
+            var mod = a % b;
+            Console.WriteLine($"{a}/{b}={div} remainder {mod}");
+            var areaOfCircle = CalculateArea(20);
+            Console.WriteLine($"The area of a circle with a radius if 20 is {CalculateArea(20)}");
+        }
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);    
+        }
+
+    
+    }
+}
+            */
+
+
+
+
         }
     }
 }
+
